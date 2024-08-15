28b904f18780b3597568ea1b035d9e37b722fddf
Joshua Kaasik <joshua.kaasik@gmail.com>
2024-08-15 09:14:58 -0500

    Added linter and autoformatter

--- lua/jikaasik/lazy.lua
+++ lua/jikaasik/lazy.lua
@@ -11,4 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("jikaasik.plugins")
+require("lazy").setup({ { import = "jikaasik.plugins" }, { import = "jikaasik.plugins.lsp" } }, {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})
--- test.py
+++
@@ -1,7 +0,0 @@
-def testfunc(x):
-    return x ** 2
-
-
-
-def testfunc2():
-    return None
